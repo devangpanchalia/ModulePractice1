4c59f76d2421ca2cf30b0c91261c27c4bc05f6e1
Devang Panchalia <devangpanchalia@gmail.com>
2023-08-28 18:02:19 -0400

    Committing firsr Changes

---
+++ MyMainProgram.py
@@ -0,0 +1,13 @@
+#My main program which will utilize the mathModule
+import mathModule
+
+print("My main program is starting")
+
+print(f"Value of __main__ is {__name__}")
+
+print("Calling the math module")
+
+print(f"Square of 3 is {mathModule.square(3)}")
+      
+print("End of main program. We demod the python modules")
+
---
+++ mathModule.py
@@ -0,0 +1,7 @@
+#this my new module which has all the functions defined. 
+
+def square(var) :
+    return var * var
+
+
+print("End of module")
